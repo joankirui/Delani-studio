af6ae219b07711cc794b5698ca3214692e8277bb
joankirui <joankirui99@gmail.com>
2019-07-20 20:21:05 +0300

    make the what we do icons to toggle between respective description of each icon when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
-    $("").click(function(){
-        $(img.service-icons).toggle();
-
-
+    $("div.show").click(function(){
+        $("#design").show();
+        $("div.show").hide();
+    });
+    $("div.show1").click(function(){
+        $("#development").show();
+        $("div.show1").hide();
+    });
+    $("div.show2").click(function(){
+        $("#product").show();
+        $("div.show2").hide();
     });
 });
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("div.show").show();
+        $("#design").hide();
+    });
+    $("#development").click(function(){
+        $("div.show1").show();
+        $("#development").hide();
+    });
+    $("#product").click(function(){
+        $("div.show2").show();
+        $("#product").hide();
+    });
+});
+
