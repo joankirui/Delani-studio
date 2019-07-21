88421de9f080ca409ef654f058c8253e678bc67a
joankirui <joankirui99@gmail.com>
2019-07-21 14:44:29 +0300

    add a class called wrap to enable the hover effect on the portfolio sections

--- js/scripts.js
+++ js/scripts.js
@@ -27,23 +27,61 @@ $(document).ready(function(){
     });
 });
 $(document).ready(function(){
-    $("img#img1").mouseover(function(){
-        $()
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $(".wrap1").mouseover(function(){
+        $("#overlay1").show();
+    }).mouseout(function(){
+        $("#overlay1").hide();
+    });
+});
+$(document).ready(function(){
+    $(".wrap2").mouseover(function(){
+        $("#overlay2").show();
+    }).mouseout(function(){
+        $("#overlay2").hide();
+    });
+});
+$(document).ready(function(){
+    $(".wrap3").mouseover(function(){
+        $("#overlay3").show();
+    }).mouseout(function(){
+        $("#overlay3").hide();
+    });
+});
+$(document).ready(function(){
+    $(".wrap4").mouseover(function(){
+        $("#overlay4").show();
+    }).mouseout(function(){
+        $("#overlay4").hide();
+    });
+});
+$(document).ready(function(){
+    $(".wrap5").mouseover(function(){
+        $("#overlay5").show();
+    }).mouseout(function(){
+        $("#overlay5").hide();
+    });
+});
+$(document).ready(function(){
+    $(".wrap6").mouseover(function(){
+        $("#overlay6").show();
+    }).mouseout(function(){
+        $("#overlay6").hide();
+    });
+});
+$(document).ready(function(){
+    $(".wrap7").mouseover(function(){
+        $("#overlay7").show();
+    }).mouseout(function(){
+        $("#overlay7").hide();
+    });
+});
+$(document).ready(function(){
+    $(".wrap8").mouseover(function(){
+        $("#overlay8").show();
+    }).mouseout(function(){
+        $("#overlay8").hide();
+    });
+});
 
 
 $(document).ready(function(){
